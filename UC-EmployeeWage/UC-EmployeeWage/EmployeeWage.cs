6a9279e51b27ffa777de41b1afd6f58d185dd725
Nanthagopal98 <nanthagopal.sj@gmail.com>
2022-05-17 06:52:59 +0530

    commiting UC-1

---
+++ UC-EmployeeWage/UC-EmployeeWage/EmployeeWage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC_EmployeeWage
+{
+    public class EmployeeWage
+    {
+        int employeePresent = 1;
+        int absent = 0;
+        public void employeeAttendatce() 
+        {
+            Random random = new Random();
+            int attendance = random.Next(2);
+            if(attendance==0)
+            {
+                Console.WriteLine("Employee present");
+            }
+            else
+            {
+                Console.WriteLine("Employee absent");
+            }
+               
+        }
+    }
+}
---
+++ UC-EmployeeWage/UC-EmployeeWage/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+UC_EmployeeWage.EmployeeWage run = new UC_EmployeeWage.EmployeeWage();
+run.employeeAttendatce();
