56be8c61bb8e590d3f429b9c4543a403e3e04b7d
Nanthagopal98 <nanthagopal.sj@gmail.com>
2022-05-17 09:31:34 +0530

    [Nantha] - UC-6-Commited

--- UC-EmployeeWage/UC-EmployeeWage/EmployeeWage.cs
+++ UC-EmployeeWage/UC-EmployeeWage/EmployeeWage.cs
@@ -8,41 +8,52 @@ namespace UC_EmployeeWage
 {
     public class EmployeeWage
     {
-        int employeeFullTimet = 1;
-        int employeePartTimet = 2;
-        int absent = 0;
         int empWagePerHour = 20;
         int isFullTime = 8;
         int isPartTime = 4;
+        int maxWorkinhHours = 100;
+        int maxWorkingDays = 20;
+        int empWage = 0;
+        int workingHour = 0;
+        int hour;
         public void employeeAttendatce() 
         {
-            int[] values = new int[20];
-            for (int i = 1; i <= 20; i++)
+            for (int i = 1; i <= maxWorkingDays; i++)
             {
-                Random random = new Random();
-                int attendance = random.Next(3);
-                switch (attendance)
+                if (workingHour < maxWorkinhHours)
                 {
-                    case 1:
-                        Console.WriteLine("Employee Fulltime");
-                        int fullTimeWage = empWagePerHour * isFullTime;
-                        Console.WriteLine("Day " + i +" Employee Wage is = " + fullTimeWage);
-                        values[i]=fullTimeWage;
-                        break;
-                    case 2:
-                        Console.WriteLine("Employee Parttime");
-                        int partTimeWage = empWagePerHour * isPartTime;
-                        Console.WriteLine("Day " + i +" Employee Wage is = " + partTimeWage);
-                        values[i]=partTimeWage;
-                        break;
-                    case 0:
-                        Console.WriteLine("Employee Absent");
-                        Console.WriteLine("Day " + i + " Employee Wage is = " + 0);
-                        values[i]=0;
-                        break;
+                    Random random = new Random();
+                    int attendance = random.Next(3);
+                    switch (attendance)
+                    {
+                        case 1:
+                            Console.WriteLine("Employee Fulltime");
+                            Console.WriteLine("Day " + i + " Employee Wage is = " + isFullTime);
+                            hour = 8;
+                            break;
+                        case 2:
+                            Console.WriteLine("Employee Parttime");
+                            Console.WriteLine("Day " + i + " Employee Wage is = " + isPartTime);
+                            hour = 4;
+                            break;
+                        case 0:
+                            Console.WriteLine("Employee Absent");
+                            Console.WriteLine("Day " + i + " Employee Wage is = " + 0);
+                            hour = 0;
+                            break;
+                    }
+                    int dailyWage = hour * empWagePerHour;
+                    empWage = empWage + dailyWage;
+                    workingHour = workingHour + hour;
+
+                }
+                else
+                {
+                    break;
                 }
             }
-               
+            Console.WriteLine("Total Employee Wage = " + empWage);
+            Console.WriteLine("Total Employee Working Hour = " + workingHour);
         }
     }
 }
