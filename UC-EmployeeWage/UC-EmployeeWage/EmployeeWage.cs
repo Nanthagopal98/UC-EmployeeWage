e6e26fa1dba1657ca8c2f72e47c05c862c0bc92f
Nanthagopal98 <nanthagopal.sj@gmail.com>
2022-05-17 07:05:03 +0530

    [Nantha] - Commiting UC-2

--- UC-EmployeeWage/UC-EmployeeWage/EmployeeWage.cs
+++ UC-EmployeeWage/UC-EmployeeWage/EmployeeWage.cs
@@ -10,13 +10,17 @@ namespace UC_EmployeeWage
     {
         int employeePresent = 1;
         int absent = 0;
+        int empWagePerHour = 20;
+        int isFullTime = 8;
         public void employeeAttendatce() 
         {
             Random random = new Random();
             int attendance = random.Next(2);
-            if(attendance==0)
+            if(attendance==1)
             {
                 Console.WriteLine("Employee present");
+                int wagePerDay = empWagePerHour * isFullTime;
+                Console.WriteLine("Employee Wage per day = " + wagePerDay);
             }
             else
             {
