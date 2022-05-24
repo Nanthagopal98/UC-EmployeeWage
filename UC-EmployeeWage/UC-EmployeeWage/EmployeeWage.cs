b1119d426c3c2d9d63bafc3ece34ada15d3543a5
Nanthagopal98 <nanthagopal.sj@gmail.com>
2022-05-24 06:34:09 +0530

    [Nantha] - UC-10-Manage_Emp_Wage Added

--- UC-EmployeeWage/UC-EmployeeWage/EmployeeWage.cs
+++ UC-EmployeeWage/UC-EmployeeWage/EmployeeWage.cs
@@ -6,53 +6,86 @@ using System.Threading.Tasks;
 
 namespace UC_EmployeeWage
 {
-    public class EmployeeWage 
+    public class bulider
     {
-        int  empWage = 0, workingHour = 0,hour;
-        string companyName;
-        public void employeeAttendatce(string companyName, int empWagePerHour, int maxWorkinhHours, int maxWorkingDays) 
-        {
-            this.companyName = companyName;
-            for (int i = 1; i <= maxWorkingDays; i++)
-            {
-                if (workingHour < maxWorkinhHours)
-                {
-                    Random random = new Random();
-                    int attendance = random.Next(3);
-                    switch (attendance)
-                    {
-                        case 1:
-                            //Console.WriteLine("Employee Fulltime");
-                            //Console.WriteLine("Day " + i + " Employee Wage is = " + isFullTime);
-                            hour = 8;
-                            break;
-                        case 2:
-                           // Console.WriteLine("Employee Parttime");
-                           // Console.WriteLine("Day " + i + " Employee Wage is = " + isPartTime);
-                            hour = 4;
-                            break;
-                        case 0:
-                           // Console.WriteLine("Employee Absent");
-                           // Console.WriteLine("Day " + i + " Employee Wage is = " + 0);
-                            hour = 0;
-                            break;
-                    }
-                    int dailyWage = hour * empWagePerHour;
-                    empWage = empWage + dailyWage;
-                    workingHour = workingHour + hour;
+        private int numberOfCompanies = 0;
+        private EmployeeWage[] companyEmpWagearray;
+        int empWage = 0, workingHour = 0, hour;
 
-                }
-                else
-                {
-                    break;
-                }
+        public bulider()
+        {
+            this.companyEmpWagearray = new EmployeeWage[5];
+        }
+
+        public void addtoArray(string companyName, int empWagePerHour, int maxWorkinhHours, int maxWorkingDays)
+        {
+            companyEmpWagearray[this.numberOfCompanies] = new EmployeeWage(companyName, empWagePerHour, maxWorkinhHours, maxWorkingDays);
+            numberOfCompanies++;
+        }
+        public void computeEmpWage()
+        {
+            for(int i = 0; i < numberOfCompanies; i++)
+            {
+                companyEmpWagearray[i].totalEmpWage((this.computeEmpWage(this.companyEmpWagearray[i])));
+                Console.WriteLine(this.companyEmpWagearray[i].companyNameAndSalary());
             }
-            Console.WriteLine("Total Employee Wage = " + empWage);
-            Console.WriteLine("Total Employee Working Hour = " + workingHour);
+        }
+        private int computeEmpWage(EmployeeWage EmployeeWage)
+        {
+            int dailyWage;
+            while (empWage <= EmployeeWage.maxWorkinhHours && workingHour <= EmployeeWage.maxWorkingDays)
+            {
+                Random random = new Random();
+                int attendance = random.Next(3);
+                switch (attendance)
+                {
+                    case 1:
+                        //Console.WriteLine("Employee Fulltime");
+                        //Console.WriteLine("Day " + i + " Employee Wage is = " + isFullTime);
+                        hour = 8;
+                        break;
+                    case 2:
+                        // Console.WriteLine("Employee Parttime");
+                        // Console.WriteLine("Day " + i + " Employee Wage is = " + isPartTime);
+                        hour = 4;
+                        break;
+                    case 0:
+                        // Console.WriteLine("Employee Absent");
+                        // Console.WriteLine("Day " + i + " Employee Wage is = " + 0);
+                        hour = 0;
+                        break;
+                }
+                workingHour += hour;
+                dailyWage = hour * EmployeeWage.empWagePerHour;
+            }
+            return empWage = workingHour * EmployeeWage.empWagePerHour;
+        }
+        
+    }
+
+    public class EmployeeWage
+    {
+        public string companyName;
+        public int empWagePerHour;
+        public int maxWorkinhHours;
+        public int maxWorkingDays;
+        public int empWage;
+
+        public EmployeeWage(string companyName, int empWagePerHour, int maxWorkinhHours, int maxWorkingDays)
+        {
+            this.companyName = companyName; ;
+            this.empWagePerHour = empWagePerHour;
+            this.maxWorkinhHours = maxWorkinhHours;
+            this.maxWorkingDays = maxWorkingDays;
         }
         public string companyNameAndSalary()
         {
-            return "Company Name : "+companyName+ "\nEmployrrSalary : "+empWage ;
+            return "Company Name : " + this.companyName + "\nEmployrrSalary : " + this.empWage;
+        }
+        public void totalEmpWage(int empWage)
+        {
+            this.empWage = empWage;
         }
     }
+
 }
--- UC-EmployeeWage/UC-EmployeeWage/Program.cs
+++ UC-EmployeeWage/UC-EmployeeWage/Program.cs
@@ -1,8 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-UC_EmployeeWage.EmployeeWage company1= new UC_EmployeeWage.EmployeeWage();
-company1.employeeAttendatce( "XYZ Company", 20, 100, 20);
-Console.WriteLine(company1.companyNameAndSalary());
-Console.WriteLine();
-UC_EmployeeWage.EmployeeWage company2 = new UC_EmployeeWage.EmployeeWage();
-company2.employeeAttendatce("ABC Company", 30, 80, 15);
-Console.WriteLine(company2.companyNameAndSalary());
+UC_EmployeeWage.bulider run = new UC_EmployeeWage.bulider();
+run.addtoArray("XYZ Company", 20,100,20);
+run.addtoArray("ABC Company", 30, 80, 15);
+run.computeEmpWage();
